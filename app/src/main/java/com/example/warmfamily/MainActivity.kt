cf1063dc69db475d8a86eb3385e4a634792045d0
hyeg0121 <hyeg0121@naver.com>
2023-07-20 16:20:33 +0900

    [feat] 액션바 숨기기

--- app/src/main/java/com/example/warmfamily/MainActivity.kt
+++ app/src/main/java/com/example/warmfamily/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()    // 상단 액션바 숨기기
     }
 }
