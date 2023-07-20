46cb411b2aeb901af675d3d39783111b7b9a6720
hyeg0121 <hyeg0121@naver.com>
2023-07-20 16:19:24 +0900

    [feat] 롤링페이퍼 조회 페이지 파일 생성

---
+++ app/src/main/java/com/example/warmfamily/RollingpaperViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.warmfamily
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RollingpaperViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.rollingpaper_view_layout)
+    }
+}
